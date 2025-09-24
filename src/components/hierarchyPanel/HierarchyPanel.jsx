568f8b6e99f8876249fd13016f424b8e99322212
noverlean <noverlin1352@gmail.com>
2025-09-24 09:43:22 +0300

    + GraphHierarchyPanel content plug ====================== - slide bar into hierarchy panel ====================== visual improvements

--- src/components/hierarchyPanel/HierarchyPanel.jsx
+++ src/components/hierarchyPanel/HierarchyPanel.jsx
@@ -6,35 +6,10 @@ import SettingHierarchyPanel from "@components/hierarchyPanel/includes/SettingHi
 export default function HierarchyPanel({ openType })
 {
     const [currentContent, setCurrentContent] = useState('');
-    // const [resizing, setResizing] = useState(false);
-    const [lastSettedWidth, setLastSettedWidth] = useState(250);
+    const panelWidth = 250;
 
     const [width, setWidth] = useState(250);
-    //
-    // function handleMouseMove(event) {
-    //     console.log(resizing)
-    //     if (resizing)
-    //         setWidth(event.clientX - 51)
-    // }
-    //
-    // function handleMouseUp() {
-    //     setResizing(false)
-    // }
-    //
-    // useEffect(() => {
-    //     console.log('change ' + resizing)
-    // }, [resizing])
-    //
-    // useEffect(() => {
-    //     document.removeEventListener('mousemove', (event) => handleMouseMove(event))
-    //     document.removeEventListener('mouseup', handleMouseUp)
-    //     //todo resizing of hierarchy panel
-    //     document.event
-    //
-    //     document.addEventListener('mousemove', handleMouseMove(event))
-    //     document.addEventListener('mouseup', handleMouseUp)
-    // }, [resizing])
-    //
+
     useEffect(() => {
         console.log(55)
         document.getElementById('hierarchyPanel').style.width = width + 'px'
@@ -48,19 +23,19 @@ export default function HierarchyPanel({ openType })
                 console.log(9090909)
                 break;
             case 'structure':
-                setWidth(lastSettedWidth)
+                setWidth(panelWidth);
                 setCurrentContent(<StructureHierarchyPanel />)
                 break;
             case 'graph':
-                setWidth(lastSettedWidth)
+                setWidth(panelWidth);
                 setCurrentContent(<StructureHierarchyPanel />)
                 break;
             case 'settings':
-                setWidth(lastSettedWidth)
+                setWidth(panelWidth);
                 setCurrentContent(<SettingHierarchyPanel />)
                 break;
         }
-    }, [lastSettedWidth, openType]);
+    }, [ openType ]);
 
     return (
         <div id='hierarchyPanel'
@@ -73,11 +48,6 @@ export default function HierarchyPanel({ openType })
             <div className='contentContainer'>
                 { currentContent }
             </div>
-
-            {/*<div className='splitter' onMouseDown={ () => {*/}
-            {/*    console.log(909090)*/}
-            {/*    setResizing(true)*/}
-            {/*}}></div>*/}
         </div>
     )
 }
---
+++ src/components/hierarchyPanel/includes/GraphHierarchyPanel.jsx
@@ -0,0 +1,8 @@
+export default function GraphHierarchyPanel()
+{
+    return (
+        <>
+            <h2 className='header'>Графы</h2>
+        </>
+    )
+}
